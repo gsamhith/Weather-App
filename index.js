b976400786f39e2eaad35e3d52b135435b6d0c17
gsamhith <reddygsamhith@gmail.com>
2024-10-18 17:31:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import bodyParser from "body-parser";
+import express from "express";
+import https from "https";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index" , {query: null, temp: null, description: null, icon: null});
+});
+
+app.post("/search", (req, res) => {
+    const query = req.body.cityName;
+    const apiKey = 'ce5f6420ac38f5a024d98294e9765c99';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ query +'&appid='+ apiKey +'&units=metric';
+    https.get(url, (response) => {
+        console.log('statusCode:', response.statusCode);
+        response.on('data', (d) => {
+            const weatherData = JSON.parse(d);
+            const weatherTemp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const weatherIcon = weatherData.weather[0].icon;
+            res.render("result", {query: query, temp: weatherTemp, description: weatherDescription, icon: weatherIcon});
+            setTimeout(() => {
+                res.redirect("/");
+            }, 3000);
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
+});
